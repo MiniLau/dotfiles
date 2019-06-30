7b452b17af229417ca09f8abad19f34b7c22dc79
MiniLau <laurent@desausoi.net>
2019-06-30 21:17:04 +0200

    Installation script

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
-#!/bin/zsh
-DOTFILES_DIR="~/.dotfiles"
-
-# ln -svf $DOTFILES_DIR/git/.gitconfig ~/.gitconfig
-ln -svf $DOTFILES_DIR/vim/.vimrc ~/.vimrc
-# ln -svf $DOTFILES_DIR/runcom/.bash_profile ~/.bash_profile
-# ln -svf $DOTFILES_DIR/runcom/.inputrc ~/.inputrc
-
-
+#!/bin/bash
+DOTFILES_DIR="/Users/laurentdesausoi/.dotfiles"
 
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #brew tap Homebrew/bundle # Install brew bundle
-#brew bundle --file=$(DOTFILES_DIR)/install/Brewfile
-#brew bundle --file=$(DOTFILES_DIR)/install/Caskfile
-#brew bundle --file=$(DOTFILES_DIR)/install/Masfile
+#brew bundle --file=$DOTFILES_DIR/install/Brewfile
+#brew bundle --file=$DOTFILES_DIR/install/Caskfile
+#brew bundle --file=$DOTFILES_DIR/install/Masfile
 
-#source $(DOTFILES_DIR)/macos/defaults.sh
-#source $(DOTFILES_DIR)/macos/dock.sh
+# Installing Oh My Zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#ln -svf $DOTFILES_DIR/git/.gitconfig ~/.gitconfig
+#ln -svf $DOTFILES_DIR/vim/.vimrc ~/.vimrc
+#ln -svf $DOTFILES_DIR/runcom/.bash_profile ~/.bash_profile
+#ln -svf $DOTFILES_DIR/runcom/.inputrc ~/.inputrc
+
+#source $DOTFILES_DIR/macos/defaults.sh
+#source $DOTFILES_DIR/macos/dock.sh
