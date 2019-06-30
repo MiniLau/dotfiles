fb13d6f85247caeccbd19d456d1e94697e67ae5a
MiniLau <laurent@desausoi.net>
2019-06-30 23:11:22 +0200

    Installation fixed + README

--- README.md
+++ README.md
@@ -6,10 +6,5 @@
 git clone https://github.com/MiniLau/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 chmod +x install.sh
-```
-
-Then you must edit the installation script to match your path. Then run it:
-
-```
 ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-DOTFILES_DIR="/Users/laurentdesausoi/.dotfiles"
+DOTFILES_DIR="/$HOME/.dotfiles"
 
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#brew tap Homebrew/bundle # Install brew bundle
-#brew bundle --file=$DOTFILES_DIR/install/Brewfile
-#brew bundle --file=$DOTFILES_DIR/install/Caskfile
-#brew bundle --file=$DOTFILES_DIR/install/Masfile
+# Installing Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap Homebrew/bundle # Install brew bundle
+brew bundle --file=$DOTFILES_DIR/install/Brewfile
+brew bundle --file=$DOTFILES_DIR/install/Caskfile
+brew bundle --file=$DOTFILES_DIR/install/Masfile
 
 # Installing Oh My Zsh
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-#ln -svf $DOTFILES_DIR/git/.gitconfig ~/.gitconfig
-#ln -svf $DOTFILES_DIR/vim/.vimrc ~/.vimrc
-#ln -svf $DOTFILES_DIR/runcom/.bash_profile ~/.bash_profile
-#ln -svf $DOTFILES_DIR/runcom/.inputrc ~/.inputrc
+ln -svf $DOTFILES_DIR/git/.gitconfig ~/.gitconfig
+ln -svf $DOTFILES_DIR/vim/.vimrc ~/.vimrc
+ln -svf $DOTFILES_DIR/runcom/.bash_profile ~/.bash_profile
+ln -svf $DOTFILES_DIR/runcom/.zshrc ~/.zshrc
+ln -svf $DOTFILES_DIR/runcom/.inputrc ~/.inputrc
 
-#source $DOTFILES_DIR/macos/defaults.sh
-#source $DOTFILES_DIR/macos/dock.sh
+source $DOTFILES_DIR/macos/defaults.sh
+source $DOTFILES_DIR/macos/dock.sh
